3ab489c9b3656459f284fb367029534167d5ffce
Alexkand <kandaurovav@mail.ru>
2022-06-22 13:13:17 +0300

    ok, random work

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,19 @@
-﻿double nunberA = 12;
-double numberB = 5;
-double c = (numberA / numberB);
+﻿double A = 12;
+
+double B = 5;
+
+double c = (A / B);
+
 Console.WriteLine(c);
+
+int D = new Random().Next(1, 21);
+
+int F = new Random().Next(1, 11);
+
+Console.WriteLine(D);
+
+Console.WriteLine(F);
+
+double E = (D / F);
+
+Console.WriteLine(E);
