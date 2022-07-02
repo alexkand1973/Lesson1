0e3b36194956d2836e63e4dfea11adfac533070a
Alexkand <kandaurovav@mail.ru>
2022-07-02 14:07:52 +0300

    Программа не работает

---
+++ HomeWork_Polindrom/Program.cs
@@ -0,0 +1,34 @@
+﻿//Проверка 5-ти значного числа, является ли оно полидромом?
+
+
+Console.WriteLine("Введите 5-ти значное число");
+
+string[] Number = new string[5];
+
+Console.ReadLine();
+
+int Length = Number.Length;
+
+int[] FerstNumber = int.TryParse(Number);
+
+int i=0;
+
+string Polidrom
+(
+  
+        if(FerstNumber[i] == FerstNumber[^1] && Number[i+1] == Number[^2])
+        {
+            Console.WriteLine("Число " + FerstNumber + " является полидромом.");  
+        }
+        else
+        Console.WriteLine("Число " + FerstNumber + " не является полидромом.");
+
+)
+
+if(int.TryParse(Number) == true)
+{
+    Polidrom(FerstNumber);
+}
+else
+onsole.WriteLine("Это не 5-ти значное число");
+
